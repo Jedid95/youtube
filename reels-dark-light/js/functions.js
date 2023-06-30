875f5271f40d6465e15f5997793386dc183702b4
Jedid95 <jedid.santos@gmail.com>
2023-06-30 20:24:01 +0100

    added reels light dark

---
+++ reels-dark-light/js/functions.js
@@ -0,0 +1,11 @@
+$(function(){
+    $(".change").on("click", function(){
+        if($("body").hasClass("dark")){
+            $("body").removeClass("dark");
+            $(".change").text("OFF");
+        }else {
+            $("body").addClass("dark");
+            $(".change").text("ON");
+        }
+    })
+})
